cdbd172d8dbde63dbb1d55a07e060381dc9cf47e
Maciej Ruciński <maciej.rucinski@3mdeb.com>
2018-05-09 16:28:34 +0200

    music.py: add symbols and selecting song

--- music.py
+++ music.py
@@ -12,15 +12,28 @@ from Tkinter import *
 from tkFileDialog import askopenfilename
 import pygame
 
+class Song():
+	def __init__(self, path):
+		self.path = path
+		self.name = self.path.split('/')[-1]
+
 class Application(Frame):
 	def say_hi(self):
 		print "hi there, everyone!"
 
 	def play(self):
 		print(self.playlist)
-		for file in self.playlist:
-			pygame.mixer.music.load(file)
+		try:
+			self.current_song = self.listbox.get(self.listbox.curselection())
+		except:
+			print('None of song is selected')
+		else:
+			pygame.mixer.music.load('./media/' + self.current_song)
 			pygame.mixer.music.play()
+			
+		# if self.current_song != None:
+		# 	pygame.mixer.music.load(self.current_song)
+		# 	pygame.mixer.music.play()
 		# while pygame.mixer.music.get_busy(): 
 		# 	print('Playing...') 	
 
@@ -31,10 +44,12 @@ class Application(Frame):
 		musicFile = askopenfilename()
 		self.playlist.append(musicFile)
 		self.listbox.insert(END, musicFile.split('/')[-1])
+		self.listbox.selection_set(first=musicFile)
+		# print('list of songs =>', self.listbox.get(self.listbox.curselection()))
 
 	def createWidgets(self):
 		self.QUIT = Button(self)
-		self.QUIT["text"] = "QUIT"
+		self.QUIT["text"] = "\xe2\x98\x93"
 		self.QUIT["fg"]   = "red"
 		self.QUIT["command"] =  self.quit
 
@@ -47,15 +62,15 @@ class Application(Frame):
 		# self.hi_there.pack({"side": "left"})
 
 		self.playButton = Button(self)
-		self.playButton["text"] = "Play"
+		self.playButton["text"] = "\xe2\x99\xaa"
 		self.playButton["fg"] = "green"
 		self.playButton["command"] = self.play
 
 		self.playButton.pack({"side": "right"})
 
 		self.stopButton = Button(self)
-		self.stopButton["text"] = "Stop"
-		self.stopButton["fg"] = "red"
+		self.stopButton["text"] = u"\u25A0"
+		self.stopButton["fg"] = "black"
 		self.stopButton["command"] = self.stop
 
 		self.stopButton.pack({"side": "right"})
@@ -69,27 +84,36 @@ class Application(Frame):
 
 		self.listbox = Listbox(
 			self.master,
-			bg='#FFBF00',
-			bd=3,
+			bg='#ffd89b',
+			bd=0.5,
 			font='DejaVuSerif-Bold'
 		)
 		# print(self.listbox[0])
 		self.listbox.pack()
 
 	def __init__(self, master=None):
+		import sys
+		mytext = u""
+		mytext += unichr(247) 
+		#check the codes for unicode chars here:  http://en.wikipedia.org/wiki/List_of_Unicode_characters
+		print mytext.encode(sys.stdout.encoding, errors="replace")
+		print('\xe2\x99\xa5')
+
 		self.master = master
 		pygame.init()
 		pygame.mixer.init()
 
+		self.current_song = None
 		self.playlist = []
 
-		master.minsize(width=400, height=200)
-		master.configure(background='#CC5D2B')
+		master.minsize(width=600, height=300)
+		master.configure(background='#ff9d00')
 		Frame.__init__(self, master)
 		self.pack()
 		self.createWidgets()
 
 root = Tk()
+root.wm_attributes('-type', 'splash')
 app = Application(master=root)
 app.mainloop()
 root.destroy()
